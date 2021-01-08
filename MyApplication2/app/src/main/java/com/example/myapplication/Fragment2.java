e95f52a96a961339201f0a837ac4ea2fe37a3ffe
woowonjin <2017029970@hanyang.ac.kr>
2021-01-09 00:14:45 +0900

    get image #4

--- MyApplication2/app/src/main/java/com/example/myapplication/Fragment2.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Fragment2.java
@@ -1,35 +1,83 @@
 package com.example.myapplication;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.Toast;
+
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 
 public class Fragment2 extends Fragment {
 
+    private static final int REQUEST_CODE = 0;
+    private ImageView image_temp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_2, container, false);
+        image_temp = view.findViewById(R.id.image_temp);
 
+        ImageButton add_btn = view.findViewById(R.id.add_btn);
+        add_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
 
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                try{
+                    InputStream in = getActivity().getContentResolver().openInputStream(data.getData());
 
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    image_temp.setImageBitmap(img);
+                    Log.d("img", ""+img);
+                }catch(Exception e)
+                {
 
+                }
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(getActivity(), "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
 }
