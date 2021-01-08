cd497533a4b8898b8e308cce85912b2c3611bbde
woowonjin <2017029970@hanyang.ac.kr>
2021-01-08 23:34:41 +0900

    fragment2 Button

--- MyApplication2/app/src/main/java/com/example/myapplication/Fragment2.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Fragment2.java
@@ -19,18 +19,11 @@ import android.widget.ListAdapter;
 
 public class Fragment2 extends Fragment {
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_2, container, false);
-
 
 
         return view;
